5177ed6a2dfd313f8caae797be4ebab63c6c0ed3
Tomasflorio <tomasflorio00@gmail.com>
2025-05-01 16:18:30 -0300

    fix /user endpoint

--- app/api/auth.py
+++ app/api/auth.py
@@ -42,29 +42,3 @@ async def login_user(data: LoginRequest, db: AsyncSession = Depends(get_db)):
         "isAdmin": user.isAdmin
     }
     return {"message": "Login successful", "user": user_response, "status": 200}
-
-@router.delete("/user/{user_id}")
-async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    user = await delete_user_router(db, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"message": "User deleted", "status": 200}
-
-class UserUpdate(BaseModel):
-    name: str | None = None
-    email: str | None = None
-    password: str | None = None
-    
-
-@router.put("/user/{user_id}")
-async def update_user(
-    user_id: int,
-    user_data: UserUpdate,
-    db: AsyncSession = Depends(get_db)
-):
-    print("User ID:", user_id)
-    updates = user_data.model_dump(exclude_unset=True)
-    user = await update_user_router(db, user_id, updates)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"message": "User updated", "user": user, "status": 200}
